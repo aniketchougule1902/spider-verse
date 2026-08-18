63a9ad8a30a08cf865170966af00c2c1182d533a
aniketchougule1902 <2.47381287e+08+aniketchougule1902@users.noreply.github.com>
2026-08-18 09:25:47 +0000

    Fix mobile observatory layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./vercel-assets.css";
+import "./mobile-observatory.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
